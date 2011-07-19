43cab63a07e2a2fbfc84dbb05d89a9a6653e8337
Xiaojun Feng <junjun80585@gmail.com>
2011-07-18 18:17:43 -0700

    uncomment admin

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 from myproject.book import views
 
 urlpatterns = patterns('',
