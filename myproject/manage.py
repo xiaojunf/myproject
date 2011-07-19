c7fbef68a3a2c27cb3b094f89b77aa71c339ff0d
Xiaojun Feng <junjun80585@gmail.com>
2011-07-18 17:35:28 -0700

    import os

--- myproject/manage.py
+++ myproject/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
 import imp
-import sys
+import sys, os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
